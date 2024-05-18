0ab19e1508a9445327c83675cc147b7c8e0b2ade
dannyb-moveo <dannyb@moveo.co.il>
2024-05-18 15:31:13 +0300

    implemented lazy component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Suspense, lazy, useState } from "react";
+// import LazyComponent from "./components/LazyComponent/LazyComponent";
+
+const LazyComponent = lazy(() =>
+  import("./components/LazyComponent/LazyComponent")
+);
 
 function App() {
+  const [componentLoaded, setComponentLoaded] = useState(false);
+
+  const handleClick = () => {
+    setComponentLoaded(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +30,17 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <button onClick={handleClick}>Load LazyComponent</button>
+
+      <Suspense
+        fallback={
+          <div style={{ height: "200px", width: "200px" }}>Loading...</div>
+        }
+      >
+        {<LazyComponent />}
+      </Suspense>
+      {/* <LazyComponent /> */}
     </div>
   );
 }
